51d8f7ebc4de56365620c8ac8e4beb4b51b52d11
Haaomas <barreauji@gmail.com>
2022-04-04 19:18:57 +0200

    First Method Done

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,4 +2,32 @@
 console.log("Be water my Friend");
 console.log("-----");
 
-//Début du code
+//Beginning of the project
+const allInput = document.querySelectorAll('input');
+const img = document.getElementById('image');
+const spanJS = document.querySelector('span');
+let color = document.getElementById('color').value;
+spanJS.style.color = `${color}`;
+
+document.addEventListener('input', () => {
+    let spacing = document.getElementById('spacing').value;
+    let blur = document.getElementById('blur').value;
+    color = document.getElementById('color').value;
+    img.style.border = `solid ${color} ${spacing}px`;
+    img.style.filter = `blur(${blur}px)`;
+    spanJS.style.color = `${color}`;
+})
+
+//TODO Try with the var() méthode
+
+// const inputs = document.querySelectorAll('.controls input');
+// console.log('inputs', inputs)
+
+// function handleUpdate() {
+//     const suffix = this.dataset.sizing || '';
+//     console.log('this', this)
+//     console.log('suffix', suffix)
+//     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+// }
+
+// inputs.forEach(input => input.addEventListener('input', handleUpdate));
