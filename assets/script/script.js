149920c4ef5c34e1595d6551e99e200f568658c3
Haaomas <barreauji@gmail.com>
2022-04-20 21:04:45 +0200

    Clean indentation

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,31 +3,31 @@ console.log("Be water my Friend");
 console.log("-----");
 
 //Beginning of the project
-const allInput = document.querySelectorAll('input');
-const img = document.getElementById('image');
-const spanJS = document.querySelector('span');
-let color = document.getElementById('color').value;
-spanJS.style.color = `${color}`;
+// const allInput = document.querySelectorAll('input');
+// const img = document.getElementById('image');
+// const spanJS = document.querySelector('span');
+// let color = document.getElementById('color').value;
+// // spanJS.style.color = `${color}`;
 
-document.addEventListener('input', () => {
-    let spacing = document.getElementById('spacing').value;
-    let blur = document.getElementById('blur').value;
-    color = document.getElementById('color').value;
-    img.style.border = `solid ${color} ${spacing}px`;
-    img.style.filter = `blur(${blur}px)`;
-    spanJS.style.color = `${color}`;
-})
+// document.addEventListener('input', () => {
+//     let spacing = document.getElementById('spacing').value;
+//     let blur = document.getElementById('blur').value;
+//     color = document.getElementById('color').value;
+//     img.style.border = `solid ${color} ${spacing}px`;
+//     img.style.filter = `blur(${blur}px)`;
+//     spanJS.style.color = `${color}`;
+// })
 
-//TODO Try with the var() méthode
+// TODO Try with the var() méthode 
 
-// const inputs = document.querySelectorAll('.controls input');
-// console.log('inputs', inputs)
+const inputs = document.querySelectorAll('.controls input');
+console.log('inputs', inputs)
 
-// function handleUpdate() {
-//     const suffix = this.dataset.sizing || '';
-//     console.log('this', this)
-//     console.log('suffix', suffix)
-//     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-// }
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    console.log('this', this)
+    console.log('suffix', suffix)
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
 
-// inputs.forEach(input => input.addEventListener('input', handleUpdate));
+inputs.forEach(input => input.addEventListener('input', handleUpdate)); $
